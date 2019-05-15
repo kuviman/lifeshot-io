60698d5fc58a41ca3880bb097e605ec2dab378e3
kuviman <kuviman@gmail.com>
2019-05-16 01:50:25 +0400

    Players die over time

--- src/common_model/mod.rs
+++ src/common_model/mod.rs
@@ -116,6 +116,7 @@ impl Player {
     pub const ACCELERATION: f32 = 15.0;
     pub const PROJECTILE_MASS_GAIN_SPEED: f32 = 0.3;
     pub const PROJECTILE_COST_SPEED: f32 = 0.1;
+    pub const DEATH_SPEED: f32 = 1.0 / 20.0;
     pub fn new() -> Self {
         Self {
             projectile: None,
@@ -129,6 +130,8 @@ impl Player {
         }
     }
     fn update(&mut self, delta_time: f32, rules: &Rules) -> Option<Projectile> {
+        self.add_mass(-Self::DEATH_SPEED * delta_time);
+
         self.entity.vel += (self.action.target_vel.clamp(1.0) * Self::MAX_SPEED - self.entity.vel)
             .clamp(Self::ACCELERATION * delta_time);
         self.entity.update(delta_time, rules);
