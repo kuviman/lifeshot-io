2bb156ee44dcc2a0386970316fb845ed0f31b638
kuviman <kuviman@gmail.com>
2019-05-19 14:03:29 +0400

    Parse args from query on web

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,24 @@ fn main() {
     trace!("Initializing");
 
     #[cfg(target_arch = "wasm32")]
-    let opts: Opts = structopt::StructOpt::from_iter(std::iter::empty::<String>());
+    let opts: Opts = structopt::StructOpt::from_iter({
+        let mut args = Vec::<String>::new();
+        args.push("lifeshot-io".to_owned()); // `Program` itself is the first arg
+        let url = stdweb::web::window()
+            .location()
+            .expect("Failed to get window.location.href")
+            .href()
+            .expect("Failed to get window.location.href");
+        let url = url::Url::parse(&url).expect("Failed to parse window.location.href");
+        for (key, value) in url.query_pairs() {
+            let key: &str = &key;
+            let value: &str = &value;
+            args.push("--".to_owned() + key);
+            args.push(value.to_owned());
+        }
+        trace!("href => args: {:?}", args);
+        args
+    });
     #[cfg(not(target_arch = "wasm32"))]
     let opts: Opts = structopt::StructOpt::from_args();
     trace!("Options used:\n{:#?}", opts);
